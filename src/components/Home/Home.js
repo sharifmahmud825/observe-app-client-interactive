66d2c2f9c0cc8e3f36ce31d0362ecf000d3e8c64
unknown <sharifmahmud825@gmail.com>
2021-11-11 08:23:41 +0600

    header added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import Header from '../Shared/Header/Header';
 const Home = () => {
   return (
     <div>
-      <h2>This is home</h2>
       <Header></Header>
     </div>
   );
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div>
-      <h2>This is Header</h2>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Link to='/home' className='nav-link'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+              </Link>
+              <Link to='/shop' className='nav-link'>
+                <Nav.Link href='#home'>Shop</Nav.Link>
+              </Link>
+              <Link to='/orders' className='nav-link'>
+                <Nav.Link href='#home'>Orders</Nav.Link>
+              </Link>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <Link className='nav-link' to='/dashboard'>
+                  <NavDropdown.Item href='#action/3.1'>
+                    Dashboard
+                  </NavDropdown.Item>
+                </Link>
+                <NavDropdown.Divider />
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
