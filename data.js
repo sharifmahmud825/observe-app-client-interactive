2d2cf96044eab2f1c8d83b72b802f7402f246e08
unknown <sharifmahmud825@gmail.com>
2021-11-11 08:12:01 +0600

    inital setup done

---
+++ data.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    id: 1,
+    name: '3 in 1 Neumeral Watch',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/n1QndJF/product-1.jpg',
+    price: 200,
+  },
+  {
+    id: 2,
+    name: 'Black & Red Neumeral',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/hDqncNz/product-4.jpg',
+    price: 220,
+  },
+  {
+    id: 3,
+    name: 'Black Dial Leather Strap',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/kqJMPKw/product-9.jpg',
+    price: 280,
+  },
+  {
+    id: 4,
+    name: 'Black Dial Neumeral',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/fprfpn1/product-5.jpg',
+    price: 310,
+  },
+  {
+    id: 5,
+    name: 'Black Metal Bracelet Strap',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/YTg1wmP/product-3.jpg',
+    price: 300,
+  },
+  {
+    id: 6,
+    name: 'Gold on Black Neumeral',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/vkmpqDd/product-8.jpg',
+    price: 320,
+  },
+  {
+    id: 7,
+    name: 'Multifunctional Black Dial',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/Fwm3xnb/product-7.jpg',
+    price: 400,
+  },
+  {
+    id: 8,
+    name: 'Classic Men',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/9Vt7ZdG/product-10.jpg',
+    price: 345,
+  },
+  {
+    id: 9,
+    name: 'Analog for Girls',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/9s0WNLL/product-6.jpg',
+    price: 230,
+  },
+  {
+    id: 10,
+    name: 'Gold Case Automatic',
+    description:
+      'Elegant looking watch for every generation. This watch represents the class of the brand and it can be desired attire.',
+    image: 'https://i.ibb.co/bWJtrNg/product-2.jpg',
+    price: 290,
+  },
+];
